07ceca5785e9d86b39b4a9637c37465fe902ebb4
Renan Boniza <renanboniza1014@gmail.com>
2026-04-25 22:57:22 +0800

    saving file completed

--- services/excel_service.py
+++ services/excel_service.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-def export_to_excel(data, output_file="utils/download/Outlook_Report.xlsx"):
+
+
+
+def export_to_excel(data, output_file="shared/download/Outlook_Report.xlsx"):
     df = pd.DataFrame(data)
     df.to_excel(output_file, index=False)
     print(f"Excel generated: {output_file}")
+
+
+
--- services/outlook_service.py
+++ services/outlook_service.py
@@ -1,35 +1,29 @@
 import win32com.client
 import os
 
+
 def outlookemails(subject_filter, download_folder):
     os.makedirs(download_folder, exist_ok=True)
-
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-
     inbox = outlook.GetDefaultFolder(6)
     messages = inbox.Items
-
     messages_found = False
-
     for message in messages:
         try:
-            # Ensure it's a real email item
+        # Ensure it's a real email item
             subject = str(getattr(message, "Subject", ""))
-
             if subject_filter.lower() in subject.lower():
                 messages_found = True
-
                 attachments = message.Attachments
-
                 for att in attachments:
                     filename = att.FileName.lower()
-
-                    if filename.endswith((".xlsx", ".xls", ".csv", ".pdf")):
-                        file_path = os.path.join(download_folder, att.FileName)
-                        att.SaveASFile(file_path)
-                        print(f"Saved file: {att.FileName}")
-
+                if filename.endswith((".xlsx", ".xls", ".csv", ".pdf")):
+                    file_path = os.path.join(download_folder, att.FileName)
+                    att.SaveASFile(file_path)
+                    print(f"Saved file: {att.FileName}")
         except Exception as e:
             print("Skipping invalid item:", e)
-
     return messages_found
+
+
+ 
